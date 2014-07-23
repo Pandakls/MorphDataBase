3ff640d20378906282a3d24fe5bc7a749ae59f2a
Pandakls <theophile.plancke@ensimag.imag.fr>
2014-07-23 20:21:41 +0200

    Item implemented but not tested

--- items/item.cpp
+++ items/item.cpp
@@ -2,6 +2,24 @@
 //Utils
 #include "GadToolKit.h"
 
-item::item()
-{
+Item::Item(){}
+
+Item Item::randomItem(){};
+
+Item Item::randomItemAtPrice(int price){
+    Item item = randomItem();
+    float ratioPrice = (float)item.getPrice/(float)price;
+    while (ratioPrice < 0.9 && ratioPrice> 1.1){
+        Item = randomItem();
+        ratioPrice = (float)item.getPrice/(float)price;
+    }
+    return item;
+}
+
+Item Item::randomItemUnderPrice(int maxPrice){
+    Item item = randomItem();
+    while (item.getPrice() > maxPrice){
+        Item = randomItem();
+    }
+    return item;
 }
--- items/item.h
+++ items/item.h
@@ -1,10 +1,28 @@
 #ifndef ITEM_H
 #define ITEM_H
 
-class item
+class Item
 {
+    enum ItemType (Armor, RangeWeapon, MeleeWeapon, Clothe, Jewel, Potion);
+
 public:
-    item();
+    Item();
+    Item randomItem();
+    Item randomItemAtPrice(int price);
+    Item randomItemUnderPrice(int maxPrice);
+
+    inline int getPrice(){return price;}
+    inline std::string getName(){return name;}
+    inline std::string getDesc(){return description;}
+
+    inline void setName(std::string n){name = n;}
+    inline void setPrice(std::string p){price = p;}
+    inline void setDescription(std::string d){description = d;}
+
+private:
+    std::string name;
+    int price;
+    std::string description;
 };
 
 #endif // ITEM_H
